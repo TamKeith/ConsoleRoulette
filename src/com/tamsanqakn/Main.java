4ec7ccdf77164dccf38e8268546c52093229c834
TamKeith <tamkeithncube@gmail.com>
2021-02-01 15:05:06 +0200

    Create Betting Menu

--- src/com/tamsanqakn/Main.java
+++ src/com/tamsanqakn/Main.java
@@ -11,6 +11,9 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static int chosenNumber;
+    private static double amount;
+
     public static void main(String[] args) throws IOException {
 
         /**
@@ -50,6 +53,88 @@ public class Main {
         System.out.println("             Welcome to the Wonderlabz Console Roulette \n");
         System.out.println("********************************************************************************");
         Scanner scanner = new Scanner(System.in);
+        for(int i=0; i<playerNames.length; i++){
+
+            System.out.print("PLEASE ENTER BETS FOR: " + playerNames[i]);
+            String playerName = playerNames[i];
+            quit = false;
+
+            while(!quit){
+
+                System.out.println();
+
+                System.out.println("Choose the type of bet: \n" +
+                        " 1 : Choose Odd Number Bet \n" +
+                        " 2 : Choose Even Number Bet \n" +
+                        " 3 : Enter a Numeric Bet \n" +
+                        " 4 : Stop taking bets for current player.");
+                boolean isANumber = scanner.hasNextInt();
+                if(isANumber){
+
+                    int choice = scanner.nextInt();
+                    boolean stopBets = false;
+                    while(!stopBets){
+
+                        if((choice == 1) || (choice == 2) || (choice == 3)) {
+                            System.out.println("Enter the amount to bet for: " + playerNames[i]);
+                            amount = scanner.nextDouble();
+                        }
+
+                        PlayerBets playerBets;
+                        switch (choice) {
+                            case 1:
+                                playerBets = new PlayerBets(playerNames[i], amount, false, true);
+                                playerBets.setBetPosition();
+                                System.out.println("Odd Number bet has been placed for " + playerNames[i]);
+                                player.get(playerNames[i]).add(playerBets);
+//                                String output = StringUtils.join(new String[]{playerNames[i], "ODD", Double.toString(playerBets.getBetAmount())}, " ");
+//                                System.out.println(output);
+                                System.out.println(playerBets.getName());
+                                String result = playerBets.getName() + " " + Constants.ODD + " " + playerBets.getBetAmount();
+                                System.out.println(result);
+                                stopBets = true;
+                                break;
+                            case 2:
+                                playerBets = new PlayerBets(playerNames[i], amount, true, true);
+                                playerBets.setBetPosition();
+                                System.out.println("Even number bet has been placed for " + playerNames[i]);
+                                player.get(playerNames[i]).add(playerBets);
+                                System.out.println(playerBets.getName());
+                                System.out.println(playerBets.getName() + " " + Constants.EVEN + " " + playerBets.getBetAmount());
+                                stopBets = true;
+                                break;
+                            case 3:
+                                System.out.println("Enter a number between 1 - 36 to bet on:");
+                                int betPosition = scanner.nextInt();
+                                playerBets = new PlayerBets(playerNames[i], amount, false, false);
+                                playerBets.setBetPosition(betPosition);
+                                player.get(playerNames[i]).add(playerBets);
+                                System.out.println(playerBets.getName());
+                                System.out.println(playerBets.getName() + " " + playerBets.getBetPosition() + " " + playerBets.getBetAmount());
+                                stopBets = true;
+                                break;
+                            case 4:
+                                System.out.println("Exiting bets for player " + playerNames[i]);
+                                System.out.println();
+                                stopBets = true;
+                                quit = true;
+                                break;
+                            default:
+                                System.out.println("Please enter a choice from 1 to 4:");
+                                break;
+                        }
+                    }
+                } else {
+                    System.out.println("Please enter a numeric choice between 1, 2 and 3");
+                }
+                scanner.nextLine();
+            }
+        }
 
     }
 }
+
+enum Constants{
+    WIN, LOSE, EVEN, ODD;
+}
+
